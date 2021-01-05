67c18944dad42294f66cdcb3acaa63430bef807a
ekko <3023493319@qq.com>
2021-01-05 18:30:52 +0800

    'xyxyxyxy'

--- wx/miniprogram/app.ts
+++ wx/miniprogram/app.ts
@@ -1,6 +1,25 @@
+import {getSetting,getUserInfo} from './utils/util';
 // app.ts
 App<IAppOption>({
-  globalData: {},
+  globalData: {
+    userInfo:new Promise((resolve,reject)=>{
+      getSetting().then(res=>{
+        if (res.authSetting['scope.userInfo']) {
+          return getUserInfo()
+        }
+        return undefined
+      }).then(res=>{
+        if(!res){
+          return 
+        }
+        resolve(res.userInfo)
+        // this.globalData.userInfo = res.userInfo
+        //   if (this.userInfoReadyCallback) {
+        //     this.userInfoReadyCallback(res)
+        //   }
+      }).catch(err=>reject(err))
+    })
+  },
   onLaunch() {
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
@@ -14,25 +33,41 @@ App<IAppOption>({
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       },
     })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+    // const setting=wx.getSetting()
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            },
-          })
-        }
-      },
-    })
+    // 获取用户信息  //串联 解决回调低于
+    // getSetting().then(res=>{
+    //   if (res.authSetting['scope.userInfo']) {
+    //     return getUserInfo()
+    //   }
+    //   return undefined
+    // }).then(res=>{
+    //   if(!res){
+    //     return 
+    //   }
+    //   this.globalData.userInfo = res.userInfo
+    //     if (this.userInfoReadyCallback) {
+    //       this.userInfoReadyCallback(res)
+    //     }
+    // })
+    // wx.getSetting({//回调函数
+    //   success: res => {
+    //     if (res.authSetting['scope.userInfo']) {
+    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+    //       wx.getUserInfo({
+    //         success: res => {
+    //           // 可以将 res 发送给后台解码出 unionId
+    //           this.globalData.userInfo = res.userInfo
+
+    //           // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //           // 所以此处加入 callback 以防止这种情况
+    //           if (this.userInfoReadyCallback) {
+    //             this.userInfoReadyCallback(res)
+    //           }
+    //         },
+    //       })
+    //     }
+    //   },
+    // })
   },
 })
--- wx/typings/index.d.ts
+++ wx/typings/index.d.ts
@@ -1,8 +1,10 @@
 /// <reference path="./types/index.d.ts" />
-
+//取消 回调函数
+//将userinfo 编程 promise 
+//用then 方法 接收结果
 interface IAppOption {
   globalData: {
-    userInfo?: WechatMiniprogram.UserInfo,
+    userInfo:Promise<WechatMiniprogram.UserInfo>,
   }
-  userInfoReadyCallback?: WechatMiniprogram.GetUserInfoSuccessCallback,
+  // userInfoReadyCallback?: WechatMiniprogram.GetUserInfoSuccessCallback,
 }
